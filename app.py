b51e44c50686d732463764e34156a53e72a5ec9b
kaushal234 <77381151+kaushal234@users.noreply.github.com>
2026-07-04 19:30:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+print("Python artifact demo")
+
+with open("results.txt","w") as file
+  file.write("Hello from github \n")
+  file.write("This file is generated dynamically \n")
+  file.write(f"Genrated Time :{datetome.now()}")
+
+print("results.txt file is created successfully")
